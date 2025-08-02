53b0b86883f8bd19f49e40e15eea292484fd40c5
rkrzy <81816761+rkrzy@users.noreply.github.com>
2025-08-02 12:18:37 +0900

    msg정보 취합해서 publish

--- src/my_first_package/my_first_package/turtle_cmd_and_pose.py
+++ src/my_first_package/my_first_package/turtle_cmd_and_pose.py
@@ -11,7 +11,7 @@ class CmdAndPose(Node):
 		self.sub_pose = self.create_subscription(Pose, '/turtle1/pose', self.callback_pose, 10)
 		self.sub_cmdvel = self.create_subscription(Twist, '/turtle1/cmd_vel', self.callback_cmd, 10)
 		self.timer_period = 1.0
-		self.publisher = self.create_publisher(CmdAndPoseVel, '/cmd_and_pose', 10)
+		self.publisher = self.create_publisher(CmdAndPose, '/cmd_and_pose', 10)
 		self.timer = self.create_timer(self.timer_period, self.timer_callback)
 		self.cmd_pose = CmdAndPoseVel()
 
@@ -21,11 +21,18 @@ class CmdAndPose(Node):
 		self.cmd_pose.linear_vel = msg.linear_velocity
 		self.cmd_pose.angular_vel = msg.angular_velocity
 		print(self.cmd_pose)
+  
 	def callback_cmd(self, msg):
 		self.cmd_pose.cmd_vel_linear = msg.linear.x
-		self.cmd_pose.cmd_vel_linear = msg.angular.z
+		self.cmd_pose.cmd_vel_angular = msg.angular.z
 	def timer_callback(self):
 		self.publisher.publish(self.cmd_pose)
+  
+	# def callback_cmd(self, msg):
+	# 	self.cmd_pose.cmd_vel_linear = msg.linear.x
+	# 	self.cmd_pose.cmd_vel_linear = msg.angular.z
+	# def timer_callback(self):
+	# 	self.publisher.publish(self.cmd_pose)
 def main(args=None):
 	rp.init(args=args)
 
